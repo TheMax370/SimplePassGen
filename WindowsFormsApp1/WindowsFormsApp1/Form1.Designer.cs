382e005f3d774970af703b0b90436b07bbb9bee1
TheMax370 <69474247+TheMax370@users.noreply.github.com>
2020-11-16 22:11:44 -0400

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TextPass = new System.Windows.Forms.Label();
+            this.Btn_Generate = new System.Windows.Forms.Button();
+            this.Btn_ClipBoard = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TextPass
+            // 
+            this.TextPass.AutoSize = true;
+            this.TextPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextPass.Location = new System.Drawing.Point(52, 33);
+            this.TextPass.Name = "TextPass";
+            this.TextPass.Size = new System.Drawing.Size(92, 24);
+            this.TextPass.TabIndex = 0;
+            this.TextPass.Text = "Password";
+            this.TextPass.Click += new System.EventHandler(this.TextPass_Click);
+            // 
+            // Btn_Generate
+            // 
+            this.Btn_Generate.Location = new System.Drawing.Point(38, 84);
+            this.Btn_Generate.Name = "Btn_Generate";
+            this.Btn_Generate.Size = new System.Drawing.Size(127, 28);
+            this.Btn_Generate.TabIndex = 1;
+            this.Btn_Generate.Text = "Generate Password";
+            this.Btn_Generate.UseVisualStyleBackColor = true;
+            this.Btn_Generate.Click += new System.EventHandler(this.Btn_Generate_Click);
+            // 
+            // Btn_ClipBoard
+            // 
+            this.Btn_ClipBoard.Location = new System.Drawing.Point(38, 135);
+            this.Btn_ClipBoard.Name = "Btn_ClipBoard";
+            this.Btn_ClipBoard.Size = new System.Drawing.Size(127, 25);
+            this.Btn_ClipBoard.TabIndex = 2;
+            this.Btn_ClipBoard.Text = "Copy To ClipBoard";
+            this.Btn_ClipBoard.UseVisualStyleBackColor = true;
+            this.Btn_ClipBoard.Click += new System.EventHandler(this.Btn_ClipBoard_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(211, 172);
+            this.Controls.Add(this.Btn_ClipBoard);
+            this.Controls.Add(this.Btn_Generate);
+            this.Controls.Add(this.TextPass);
+            this.Name = "Form1";
+            this.Text = "Password Generator!";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label TextPass;
+        private System.Windows.Forms.Button Btn_Generate;
+        private System.Windows.Forms.Button Btn_ClipBoard;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void TextPass_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn_Generate_Click(object sender, EventArgs e)
+        {
+           string[] chars = new string[] { "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "!", "@", "#", "$", "%", "^", "&", "*", "-", "_", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B", "N", "M" };
+            Random random = new Random();
+            StringBuilder str = new StringBuilder();
+
+            for (int i = 0; i < 12; i++)
+            {
+                string password = chars[random.Next(i, chars.Length)];
+                TextPass.Text = str.Append(password).ToString();
+            }
+
+        }
+
+        private void Btn_ClipBoard_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(TextPass.Text);
+        }
+    }
+}
